d1400cba789bfbb82d74c0790759d922a8d3c7d2
Soophiya <sophie.grenier1117@gmail.com>
2022-12-08 20:21:49 -0500

    idk which documents to use wtf

--- exercice.py
+++ exercice.py
@@ -4,6 +4,9 @@
 PERCENTAGE_TO_LETTER = {"A*": [95, 101], "A": [90, 95], "B+": [85, 90], "B": [80, 85], "C+": [75, 80], "C": [70, 75], "F": [0, 70]}
 
 # TODO: Importez vos modules ici
+def compare_docs(doc1,doc2):
+
+    
 
 
 # TODO: Définissez vos fonction ici
